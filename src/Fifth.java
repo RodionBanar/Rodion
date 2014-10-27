5c3aa677580cb20580025e084445cdde11934b72
RodionBanar <Rodionxx@gmaill.com>
2014-10-27 16:18:39 +0300

    Update Fifth.java

--- src/Fifth.java
+++ src/Fifth.java
@@ -4,7 +4,7 @@ public class Fifth {
 
 		public static void main(String[] args) {
 			Scanner in = new Scanner(System.in);
-			System.out.print("Enter number: ");
+			System.out.print("Enter number (Edit by GitHub): ");
 			int number = Integer.parseInt(in.nextLine());
 			
 			recurse(number);
